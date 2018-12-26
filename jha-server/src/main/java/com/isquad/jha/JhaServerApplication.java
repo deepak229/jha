503b5eac140159c8beea4fda94203242ebee5224
Narasimha Deepak <narasimha.s@workday.com>
2018-12-26 12:01:56 -0800

    jha server

---
+++ jha-server/src/main/java/com/isquad/jha/JhaServerApplication.java
@@ -0,0 +1,14 @@
+package com.isquad.jha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhaServerApplication.class, args);
+    }
+
+}
+
---
+++ jha-server/src/test/java/com/isquad/jha/JhaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.isquad.jha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JhaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
